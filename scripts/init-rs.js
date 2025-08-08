0a15370f18b9447438df02fae53820a3754cf4c0
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2025-08-08 09:28:09 -0600

    cambios desde el servidor arreglando detalles de replicacion

--- scripts/init-rs.js
+++ scripts/init-rs.js
@@ -1,28 +1,8 @@
-function memberExists(host) {
-  const status = rs.status();
-  return status.members.some(m => m.name === host);
-}
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "www.atenasoficial.com:27100" },
+    { _id: 1, host: "18.217.230.121:27018" }
+  ]
+});
 
-try {
-  rs.status();
-  print("Replica Set ya inicializado.");
-
-  // Define el nuevo nodo que quieres agregar
-  const newMember = "18.217.230.121:27018";
-
-  if (!memberExists(newMember)) {
-    print("Agregando nuevo miembro:", newMember);
-    rs.add(newMember);
-  } else {
-    print("Miembro ya existe:", newMember);
-  }
-
-} catch (e) {
-  print("Replica Set no inicializado, iniciando...");
-  rs.initiate({
-    _id: "rs0",
-    members: [
-      { _id: 0, host: "www.atenasoficial.com:27100" }
-    ]
-  });
-}
