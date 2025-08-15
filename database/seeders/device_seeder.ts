9f21a9afa49541823f03077146757ac6beca60ac
KoeEmilio <jesusolvera355@gmail.com>
2025-08-14 18:47:10 -0600

    cambios

--- database/seeders/device_seeder.ts
+++ database/seeders/device_seeder.ts
@@ -8,27 +8,27 @@ export default class DeviceSeeder extends BaseSeeder {
       {
         name: 'Monitor Sala Principal',
         apiKey: '550e8400-e29b-41d4-a716-446655440001',
-        idEnvironment: 1 // Asumiendo que existe un environment con ID 1
+         // Asumiendo que existe un environment con ID 1
       },
       {
         name: 'Sensor Comedor',
         apiKey: '550e8400-e29b-41d4-a716-446655440002',
-        idEnvironment: 1
+        
       },
       {
         name: 'Control Dormitorio',
         apiKey: '550e8400-e29b-41d4-a716-446655440003',
-        idEnvironment: 2 // Asumiendo que existe un environment con ID 2
+         // Asumiendo que existe un environment con ID 2
       },
       {
         name: 'Monitor Jardín',
         apiKey: '550e8400-e29b-41d4-a716-446655440004',
-        idEnvironment: 2
+        
       },
       {
         name: 'Estación Baño',
         apiKey: '550e8400-e29b-41d4-a716-446655440005',
-        idEnvironment: 3 // Asumiendo que existe un environment con ID 3
+         // Asumiendo que existe un environment con ID 3
       }
     ])
 
